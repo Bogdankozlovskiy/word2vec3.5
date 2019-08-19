92789d1129e0de53dd11e389b36153e5dae84d23
Bogdankozlovskiy <bkozlovskiy@bk.ru>
2019-08-20 00:02:05 +0300

    start

---
+++ word2.py
@@ -0,0 +1,19 @@
+from pickle import load
+from gensim.models.word2vec import Word2Vec
+print('pleace wait a minute')
+model = load(open('word2vec', 'rb'))
+print('ready to input')
+while True:
+	positive = input('pleace input positive word if you wont) write with space  ').split()
+	negative = input('pleace input negative word if you wont) write with space  ').split()
+	if len(negative) and len(positive):
+		out = model.wv.most_similar(positive=positive, negative=negative)
+	elif len(positive):
+		out = model.wv.most_similar(positive=positive)
+	else:
+		out = model.wv.most_similar(negative=negative)
+
+	out = model.wv.most_similar(positive=['любовь', "мир"])
+	
+	for o in out:
+		print(o[0])
